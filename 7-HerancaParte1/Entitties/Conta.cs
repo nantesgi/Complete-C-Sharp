944b8e7da9e0c00b6d78456f2306d49ab454851f
Giovanna Coelho <73763043+nantesgi@users.noreply.github.com>
2022-05-30 16:28:34 -0400

    Add files via upload

---
+++ 7-HerancaParte1/Entitties/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_HerancaParte1.Entitties
+{
+    public class Conta
+    {
+        public int Numero { get; protected set; }
+        public string Titular { get; protected set; }
+        public double Saldo { get; protected set; }
+
+        public Conta()
+        {
+        }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular; 
+            Saldo = saldo;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+    }
+}
---
+++ 7-HerancaParte1/Entitties/ContaJuridica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_HerancaParte1.Entitties
+{
+    public class ContaJuridica : Conta
+    {
+        public double LimiteEmprestimo { get; set; }
+
+        public ContaJuridica()
+        {
+        }
+
+        public ContaJuridica(int numero, string titular, double saldo, double limiteEmprestimo) : base(numero, titular, saldo)
+        {
+            LimiteEmprestimo = limiteEmprestimo;
+        }
+
+        public void Emprestimo(double quantia)
+        {
+            if (quantia <= LimiteEmprestimo)
+            {
+                Saldo += quantia;
+            }
+        }
+    }
+}
